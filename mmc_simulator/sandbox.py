6a353d1cf354d9dba47d42dba60e5a60893c1d5e
Kjell Keune <k.keune@tu-braunschweig.de>
2023-06-08 19:37:55 +0200

    video of simulator

--- mmc_simulator/sandbox.py
+++ mmc_simulator/sandbox.py
@@ -7,7 +7,9 @@ Runtime user interaction is also possible. Look in simulation.py method __userIn
 
 @author: Aaron T Becker, Kjell Keune
 """
-from com.state import Configuration
+import math
+from com.state import Configuration, Cube
+from com.motion import PivotWalk, Rotation
 from sim.simulation import Simulation
 
 def sandbox_onMac():
@@ -19,5 +21,32 @@ def sandbox():
     sim1.loadConfig(Configuration((1000,1000), 0, {}))
     sim1.start()
 
+def mmc_video():
+    sim = Simulation()
+    # initial cubes
+    cubes = {
+            Cube(1): (17 *Cube.RAD, 1.3 * Cube.RAD),
+            Cube(1): (1.3 *Cube.RAD, 11 * Cube.RAD),
+            Cube(0): (8.5 *Cube.RAD, 11 * Cube.RAD),
+            Cube(0): (11 *Cube.RAD, 19 * Cube.RAD)
+    }
+    # motions
+    motions = []
+    for i in range(15):
+        motions.append(PivotWalk(PivotWalk.LEFT, math.radians(15)))
+    motions.append(Rotation(-math.radians(90)))
+    for i in range(11):
+        motions.append(PivotWalk(PivotWalk.LEFT, math.radians(15)))
+    # sequence like video
+    config = Configuration((22 * Cube.RAD, 22 * Cube.RAD), math.radians(90), cubes)
+    sim.loadConfig(config)
+    sim.start()
+    sim.stop()
+    input()
+    sim.start()
+    for motion in motions:
+        sim.executeMotion(motion)
+    
+
 if __name__ == "__main__":
-    sandbox()
+    mmc_video()
